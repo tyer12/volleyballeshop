68ea24b146e68bc262c8d99e08c341012a811cdf
tyer12 <goncalosantos2099@gmail.com>
2025-08-07 15:18:44 +0100

    End of Section 8 - Header and Observables

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/products';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class App {
-  title = 'client';
+
+export class App implements OnInit {
+  baseURl = 'http://localhost:5135/api/'
+  private http = inject(HttpClient);
+  title = 'Volleyball eShop';
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseURl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error:error => console.log (error),
+      complete: () => console.log('Product fetch operation completed.')
+    });
+  }
+  
 }
---
+++ client/src/app/shared/models/products.ts
@@ -0,0 +1,10 @@
+export type Product = {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+    type: string;
+    brand: string;
+    quantityInStock: number;
+}
