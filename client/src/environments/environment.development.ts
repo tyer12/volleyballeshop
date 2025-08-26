c000a4a64cc40a3c8fb1e43d13e9186be60cfbe8
tyer12 <goncalosantos2099@gmail.com>
2025-08-26 10:10:33 +0100

    End of Section 18 - Orders & Payments +some errors with stripe

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: false,
     apiUrl: 'https://localhost:5001/api/',
+    hubUrl: 'https://localhost:5001/hub/notifications',
     stripePublicKey: 'pk_test_51Rw1EOLW7b3TxGIRAonJGBxyzpE55sOvsAUEGXlYrhFPZAfez2DJR6vQE0S0br6jrAKFo271E5AQmZsj9M9DLuck00l09EjLGr'
+
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     apiUrl: 'api/',
+    hubUrl: 'notifications',
     stripePublicKey: 'pk_test_51Rw1EOLW7b3TxGIRAonJGBxyzpE55sOvsAUEGXlYrhFPZAfez2DJR6vQE0S0br6jrAKFo271E5AQmZsj9M9DLuck00l09EjLGr'
 };
     
